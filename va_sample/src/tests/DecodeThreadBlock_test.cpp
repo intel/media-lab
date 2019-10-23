99d872988eebb3e1396a28cb9a12d2839183e5aa
Andrey Larionov <andrey.larionov@intel.com>
2019-10-23 17:21:16 +0000

    Added more controls in CMD Changed CMDs options parsing.
    Signed-off-by: Andrey Larionov <andrey.larionov@intel.com>

--- va_sample/src/tests/DecodeThreadBlock_test.cpp
+++ va_sample/src/tests/DecodeThreadBlock_test.cpp
@@ -18,13 +18,18 @@
 #include <mfxvideo++.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <iostream>
 #include <string>
 #include <sstream>
 
 static int vp_ratio = 1;
 static int channel_num = 1;
+static int sfc_switch = 0;
+static int vpp_w = 300;
+static int vpp_h = 300;
 std::string input_filename;
+static int vpp_output_fourcc = MFX_FOURCC_RGBP;
 static bool dump_vp = false;
 
 void App_ShowUsage(void)
@@ -35,35 +40,56 @@ void App_ShowUsage(void)
     printf("                        0 means no vp\n");
     printf("           -c channel_number: number of decoding channels, default value is 1\n");
     printf("           -d dump the VP output, default value is off\n");
+    printf("           -sfc_switch: values 0,1,2. 0 - disable sfc, 1 - enable sfc. 2 - enable sfc for half streams\n");
+    printf("           -w_vpp <val> -h_vpp <val>. Target weight & height for VPP resize\n");
+    printf("           -vpp_output_format. Output format for VPP: nv12, rgbp, rgb4 \n");
 }
 
 int ParseOpt(int argc, char *argv[])
 {
-    int c;
-    while ((c = getopt (argc, argv, "c:i:r:d")) != -1)
+    if (argc < 2)
     {
-    switch (c)
-      {
-      case 'c':
-        channel_num = atoi(optarg);
-        break;
-      case 'i':
-        input_filename = optarg;
-        break;
-      case 'r':
-        vp_ratio = atoi(optarg);
-        break;
-      case 'd':
-        dump_vp = true;
-        break;
-      case 'h':
         App_ShowUsage();
         exit(0);
-      default:
-        abort ();
-      }
     }
-    
+    std::vector <std::string> sources;
+    std::string arg = argv[1];
+    if ((arg == "-h") || (arg == "--help"))
+    {
+        App_ShowUsage();
+        exit(0);
+    }
+    for (int i = 1; i < argc; ++i)
+        sources.push_back(argv[i]);
+
+    for (int i = 0; i < argc-1; ++i)
+    {
+        if (sources.at(i) == "-c")
+            channel_num = stoi(sources.at(++i));
+        else if (sources.at(i) == "-i")
+            input_filename = sources.at(++i);
+        else if (sources.at(i) == "-r")
+            vp_ratio = stoi(sources.at(++i));
+        else if (sources.at(i) == "-d")
+            dump_vp = true;
+        else if (sources.at(i) == "-w_vpp")
+            vpp_w = stoi(sources.at(++i));
+        else if (sources.at(i) == "-h_vpp")
+            vpp_h = stoi(sources.at(++i));
+        else if (sources.at(i) == "-sfc_switch")
+            sfc_switch = stoi(sources.at(++i));
+        else if (sources.at(i) == "-vpp_output_format")
+        {
+            std::string format = sources.at(++i);
+            if (format == "nv12")
+                vpp_output_fourcc = MFX_FOURCC_NV12;
+            else if (format == "rgbp")
+                vpp_output_fourcc = MFX_FOURCC_RGBP;
+            else if (format == "rgb4")
+                vpp_output_fourcc = MFX_FOURCC_RGB4;
+        }
+    }
+
     if (input_filename.empty())
     {
         printf("Missing input file name!!!!!!!\n");
@@ -92,13 +118,23 @@ int main(int argc, char *argv[])
 
         t->SetDecodeOutputRef(0);
         t->SetVPOutputRef(0);
-        //t->SetDecPostProc(true);
+        if (0 == sfc_switch)  // SFC resize off
+            t->SetDecPostProc(false);
+        else if (1 == sfc_switch) // SFC resize on
+            t->SetDecPostProc(true);
+        else if (2 == sfc_switch) // SFC resize on for half streams
+        {
+            if (1 == (i%2))
+                t->SetDecPostProc(true);
+        }
 
         t->SetVPRatio(vp_ratio);
 
-        t->SetVPOutResolution(300, 300);
+        t->SetVPOutFormat(vpp_output_fourcc);
 
-        //t->SetVPOutDump();
+        t->SetVPOutResolution(vpp_w, vpp_h);
+
+        t->SetVPOutDump(dump_vp);
 
         t->Prepare();
     }
