06e03140265cae099e35cedfa7eacb1330fab5b1
HeFan <fan.f.he@intel.com>
2019-10-24 09:10:54 +0800

    Add the setup environment script

---
+++ va_sample/env.sh
@@ -0,0 +1,10 @@
+
+export OPENVINO_DIR=/opt/intel/openvino
+#for older openvino releases
+#export OPENVINO_DIR=/opt/intel/computer_vision_sdk
+
+source $OPENVINO_DIR/bin/setupvars.sh
+
+export MFX_HOME=/opt/intel/mediasdk
+
+
