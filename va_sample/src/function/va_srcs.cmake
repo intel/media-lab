2e3928da1f49c0757efa2056ea870d3271e6d4db
HeFan <fan.f.he@intel.com>
2019-11-15 05:04:33 +0800

    Unify the license header

--- va_sample/src/function/va_srcs.cmake
+++ va_sample/src/function/va_srcs.cmake
@@ -1,3 +1,4 @@
+#
 # Copyright (c) 2019, Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
@@ -17,6 +18,7 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
+#
 
 set(VA_SOURCES
     ${VA_SOURCES}
